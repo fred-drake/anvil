14e8eef3826ce45ac61495c2d3c0cd06d60e1084
Fred Drake <fred.drake@gmail.com>
2026-07-07 01:24:28 -0400

    chore: update nix dev shell branding

--- flake.nix
+++ flake.nix
@@ -1,43 +1,44 @@
 {
-  description = "pi coding agent extension development shell";
+  description = "anvil pi coding agent extension development shell";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
 
-  outputs = { nixpkgs, ... }:
-    let
-      systems = [
-        "aarch64-darwin"
-        "x86_64-darwin"
-        "aarch64-linux"
-        "x86_64-linux"
-      ];
+  outputs = {nixpkgs, ...}: let
+    systems = [
+      "aarch64-darwin"
+      "x86_64-darwin"
+      "aarch64-linux"
+      "x86_64-linux"
+    ];
 
-      forAllSystems = nixpkgs.lib.genAttrs systems;
-    in
-    {
-      devShells = forAllSystems (system:
-        let
-          pkgs = import nixpkgs { inherit system; };
-        in
-        {
-          default = pkgs.mkShell {
-            packages = with pkgs; [
-              nodejs_24
-              git
-              direnv
-            ];
+    forAllSystems = nixpkgs.lib.genAttrs systems;
+  in {
+    devShells = forAllSystems (system: let
+      pkgs = import nixpkgs {inherit system;};
+    in {
+      default = pkgs.mkShell {
+        packages = with pkgs; [
+          nodejs_24
+          git
+          direnv
+        ];
 
-            shellHook = ''
-              export PATH="$PWD/node_modules/.bin:$PATH"
+        shellHook = ''
+          export PATH="$PWD/node_modules/.bin:$PATH"
 
-              echo "pi extension dev shell"
-              echo "  npm install        # install TypeScript and pi packages"
-              echo "  npm run typecheck  # type-check the extension"
-              echo "  npm run dev        # launch pi with this extension"
-            '';
-          };
-        });
-    };
+          cat ./assets/anvil-logo.txt
+
+          echo ""
+          echo "anvil pi extension dev shell"
+          echo ""
+          echo "  npm install        # install TypeScript and pi packages"
+          echo "  npm run typecheck  # type-check the extension"
+          echo "  npm run dev        # launch pi with this extension"
+          echo ""
+        '';
+      };
+    });
+  };
 }
